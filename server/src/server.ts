6fdebc8eeac551861259a86cbd14334030b17546
Jack Hermanson <jack.hermanson@live.com>
2021-08-11 22:40:28 -0600

    set up server.ts

---
+++ server/src/server.ts
@@ -0,0 +1,74 @@
+import express from "express";
+import bodyParser from "body-parser";
+import * as http from "http";
+import * as socketio from "socket.io";
+import path from "path";
+import { config } from "dotenv";
+import { ConnectionOptions, createConnection } from "typeorm";
+import sslRedirect from "heroku-ssl-redirect";
+import { DbDialect } from "jack-hermanson-ts-utils";
+
+// env
+const envPath = path.join(__dirname, "..", ".env");
+config({ path: envPath });
+
+// express server
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.set("port", process.env.PORT || 5000);
+
+// ssl
+app.use(sslRedirect(["production"]));
+
+// static
+const staticFiles = express.static(path.join(__dirname, "../../client/build"));
+app.use(staticFiles);
+
+// routes
+
+// database
+const databaseDialect = process.env.DATABASE_DIALECT as DbDialect;
+console.log({ databaseDialect });
+export const dbOptions: ConnectionOptions = {
+    database: databaseDialect === "sqlite" ? "site.db" : "",
+    type: databaseDialect,
+    url: process.env.DATABASE_URL,
+    entities: [],
+    synchronize: false,
+    extra: {
+        ssl: {
+            rejectUnauthorized: false,
+        },
+    },
+    migrationsRun: true,
+    migrationsTableName: "migrations",
+    migrations: [],
+    cli: {
+        migrationsDir: path.join(__dirname, "migrations"),
+    },
+};
+createConnection(dbOptions)
+    .then(connection => {
+        console.log(
+            `Connected to database with type: ${connection.options.type}.`
+        );
+    })
+    .catch(error => {
+        console.error(error);
+    });
+
+// http server and socket
+const server = http.createServer(app);
+const io = new socketio.Server({
+    cors: {
+        origin: "*",
+    },
+});
+io.attach(server);
+app.set("socketio", io);
+
+// listen
+server.listen(app.get("port"), () => {
+    console.log(`Server is listening on port ${app.get("port")}.`);
+});

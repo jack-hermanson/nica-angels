e48e7c65eb18786f4f481abdd44e3b25de4a2e68
Jack Hermanson <jack.hermanson@live.com>
2021-08-15 13:07:22 -0600

    add token model

--- server/src/migrations/_migrations.ts
+++ server/src/migrations/_migrations.ts
@@ -2,10 +2,12 @@ import { Account1628820578982 } from "./1628820578982-Account";
 import { Sponsor1628822369182 } from "./1628822369182-Sponsor";
 import { Town1628894029027 } from "./1628894029027-Town";
 import { School1628898105882 } from "./1628898105882-School";
+import { Token1629053978130 } from "./1629053978130-Token";
 
 export const migrations = [
     Account1628820578982,
     Sponsor1628822369182,
     Town1628894029027,
     School1628898105882,
+    Token1629053978130,
 ];

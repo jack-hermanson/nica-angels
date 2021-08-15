efe2da42fcd0c6ce6240244c80fe89b186881b8e
Jack Hermanson <jack.hermanson@live.com>
2021-08-15 15:46:57 -0600

    school model

---
+++ server/src/models/School.ts
@@ -0,0 +1,29 @@
+import {
+    Column,
+    CreateDateColumn,
+    UpdateDateColumn,
+    DeleteDateColumn,
+    Entity,
+    PrimaryGeneratedColumn,
+} from "typeorm";
+
+@Entity({ name: "school" })
+export class School {
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column({ nullable: false })
+    name!: string;
+
+    @Column({ nullable: false })
+    townId!: number;
+
+    @CreateDateColumn()
+    created!: Date;
+
+    @UpdateDateColumn()
+    updated!: Date;
+
+    @DeleteDateColumn()
+    deletedAt?: Date;
+}

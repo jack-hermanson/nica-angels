975f95b2dabd6a818b11633d3006a91a6145bc55
Jack Hermanson <jack.hermanson@live.com>
2021-08-29 14:41:05 -0600

    simple routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,18 +2,18 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./css/main.css";
 import { Layout } from "./components/Layout/Layout";
-import { useStoreState } from "./store/_store";
+import { AccountPage } from "./components/Account/AccountPage";
+import { DashboardPage } from "./components/Dashboard/DashboardPage";
 
 export const App: React.FC = () => {
-    const spanish = useStoreState(state => state.spanish);
-
     return (
         <BrowserRouter>
-            <Layout>{renderPlaceholder()}</Layout>
+            <Layout>
+                <Switch>
+                    <Route exact path="/" component={DashboardPage} />
+                    <Route exact path="/account" component={AccountPage} />
+                </Switch>
+            </Layout>
         </BrowserRouter>
     );
-
-    function renderPlaceholder() {
-        return <p>{spanish ? "Ésta es la aplicación." : "This is the app."}</p>;
-    }
 };
---
+++ client/src/components/Account/AccountPage.tsx
@@ -0,0 +1,24 @@
+import { useEffect, FunctionComponent } from "react";
+import { useStoreState } from "../../store/_store";
+import { useMinClearance } from "../../utils/useMinClearance";
+import { PageHeader } from "jack-hermanson-component-lib";
+import { Col, Row } from "reactstrap";
+
+export const AccountPage: FunctionComponent = () => {
+    const spanish = useStoreState(state => state.spanish);
+
+    return (
+        <div>
+            <Row>
+                <Col>
+                    <PageHeader title={spanish ? "Cuenta" : "Account"} />
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <p>Account page</p>
+                </Col>
+            </Row>
+        </div>
+    );
+};

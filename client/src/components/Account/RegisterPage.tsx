f693731400041fc2f35f84150d7fdc8fe30567a3
Jack Hermanson <jack.hermanson@live.com>
2021-08-29 15:36:38 -0600

    basic registration form

--- client/src/components/Account/RegisterPage.tsx
+++ client/src/components/Account/RegisterPage.tsx
@@ -1,8 +1,20 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useEffect } from "react";
 import { Col, Row } from "reactstrap";
 import { PageHeader } from "jack-hermanson-component-lib";
+import { useStoreState } from "../../store/_store";
+import { useHistory } from "react-router-dom";
+import { RegisterForm } from "./RegisterForm";
 
 export const RegisterPage: FunctionComponent = () => {
+    const currentUser = useStoreState(state => state.currentUser);
+    const history = useHistory();
+
+    useEffect(() => {
+        if (currentUser) {
+            history.replace("/account");
+        }
+    }, [currentUser, history]);
+
     return (
         <div>
             <Row>
@@ -11,8 +23,16 @@ export const RegisterPage: FunctionComponent = () => {
                 </Col>
             </Row>
             <Row>
-                <Col>
-                    <p>Register</p>
+                <Col xs={12} lg={6}>
+                    <RegisterForm
+                        onSubmit={async registerRequest => {
+                            console.log(registerRequest);
+                        }}
+                    />
+                </Col>
+                <Col xs={12} lg={6}>
+                    <hr className="d-lg-none mt-4" />
+                    <p>Already have an account?</p>
                 </Col>
             </Row>
         </div>

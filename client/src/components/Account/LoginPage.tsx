3068d519897dab17a2e0e3227db7bee38f8ec8ac
Jack Hermanson <jack.hermanson@live.com>
2021-09-01 19:41:14 -0600

    login form

--- client/src/components/Account/LoginPage.tsx
+++ client/src/components/Account/LoginPage.tsx
@@ -2,6 +2,7 @@ import { FunctionComponent } from "react";
 import { Col, Row } from "reactstrap";
 import { PageHeader } from "jack-hermanson-component-lib";
 import { Link } from "react-router-dom";
+import { LoginForm } from "./LoginForm";
 
 export const LoginPage: FunctionComponent = () => {
     return (
@@ -12,8 +13,17 @@ export const LoginPage: FunctionComponent = () => {
                 </Col>
             </Row>
             <Row>
-                <Col>
-                    <p>Log in.</p>
+                <Col xs={12} lg={6}>
+                    <LoginForm
+                        onSubmit={async loginRequest => {
+                            try {
+                                console.log(loginRequest);
+                            } catch (error) {
+                                console.error(error);
+                            }
+                        }}
+                    />
+                    <hr className="mt-4" />
                     <p>
                         Need an account?{" "}
                         <Link to="/account/register">Register.</Link>

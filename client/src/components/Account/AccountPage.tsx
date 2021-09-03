1b325a97d9a81367f13355dc526931a967f4ad23
Jack Hermanson <jack.hermanson@live.com>
2021-09-03 10:56:25 -0600

    user details page

--- client/src/components/Account/AccountPage.tsx
+++ client/src/components/Account/AccountPage.tsx
@@ -1,14 +1,33 @@
-import { FunctionComponent } from "react";
+import { Fragment, FunctionComponent, useEffect, useState } from "react";
 import { useStoreState } from "../../store/_store";
 import { useMinClearance } from "../../utils/useMinClearance";
-import { PageHeader } from "jack-hermanson-component-lib";
+import { LoadingSpinner, PageHeader } from "jack-hermanson-component-lib";
 import { Col, Row } from "reactstrap";
+import * as timeago from "timeago.js";
+import { tokenExpiration } from "../../../../shared/constants";
+import { UserDetails } from "../User/UserDetails";
 
 export const AccountPage: FunctionComponent = () => {
     const spanish = useStoreState(state => state.spanish);
     const token = useStoreState(state => state.token);
+    const currentUser = useStoreState(state => state.currentUser);
+
+    const [created, setCreated] = useState<Date | undefined>(undefined);
+    const [expires, setExpires] = useState<Date | undefined>(undefined);
 
     useMinClearance();
+
+    useEffect(() => {
+        if (token) {
+            const creationDate = new Date(token.created);
+            const expirationDate = new Date(
+                creationDate.getTime() + tokenExpiration
+            );
+
+            setCreated(creationDate);
+            setExpires(expirationDate);
+        }
+    }, [token, setCreated, setExpires, tokenExpiration]);
 
     return (
         <div>
@@ -19,13 +38,30 @@ export const AccountPage: FunctionComponent = () => {
             </Row>
             <Row>
                 <Col>
-                    <dl>
-                        <dt>Token</dt>
-                        <dd>{token?.data}</dd>
+                    {currentUser ? (
+                        <UserDetails user={currentUser} />
+                    ) : (
+                        <LoadingSpinner />
+                    )}
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    {!created || !expires ? (
+                        <LoadingSpinner />
+                    ) : (
+                        <Fragment>
+                            <dl>
+                                <dt>Token Created</dt>
+                                <dd>{timeago.format(created)}</dd>
 
-                        <dt>Date</dt>
-                        <dd>{token?.created}</dd>
-                    </dl>
+                                <dt>Token Expires</dt>
+                                <dd>
+                                    {`${expires.toLocaleDateString()} at ${expires.toLocaleTimeString()}`}
+                                </dd>
+                            </dl>
+                        </Fragment>
+                    )}
                 </Col>
             </Row>
         </div>

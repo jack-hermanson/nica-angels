7b8139c6acfd71ed077099f08352ff1ac60a3657
Jack Hermanson <jack.hermanson@live.com>
2021-09-03 22:06:01 -0600

    socket connection

--- client/src/components/SocketConnection.tsx
+++ client/src/components/SocketConnection.tsx
@@ -1,10 +1,25 @@
 import { Fragment, useEffect, FunctionComponent } from "react";
 import { io, Socket } from "socket.io-client";
+import { useStoreState } from "../store/_store";
 
 export const SocketConnection: FunctionComponent = () => {
+    const token = useStoreState(state => state.token);
+
     useEffect(() => {
         const socket: Socket = io("/");
-    }, []);
+
+        socket.on("connect", () => {
+            console.log("socket connected on front end");
+            if (token) {
+                console.log("token available - fetching data");
+            }
+        });
+
+        socket.on("disconnect", message => {
+            console.log("disconnected from socket");
+            console.log(message);
+        });
+    }, [token]);
 
     return <Fragment />;
 };

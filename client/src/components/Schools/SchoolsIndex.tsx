bffad0a721088c6ad9329105346cd40c15f8111a
Jack Hermanson <jack.hermanson@live.com>
2021-09-08 22:34:50 -0600

    start schools index

--- client/src/components/Schools/SchoolsIndex.tsx
+++ client/src/components/Schools/SchoolsIndex.tsx
@@ -1,22 +1,34 @@
 import { FunctionComponent } from "react";
 import { PageHeader } from "jack-hermanson-component-lib";
-import { Col, Row } from "reactstrap";
+import { Button, Col, Row } from "reactstrap";
 import { useMinClearance } from "../../utils/useMinClearance";
 import { Clearance } from "../../../../shared/enums";
 import { useStoreState } from "../../store/_store";
+import { NEW_BUTTON_COLOR } from "../../utils/constants";
 
 export const SchoolsIndex: FunctionComponent = () => {
     useMinClearance(Clearance.SPONSOR);
 
     const spanish = useStoreState(state => state.spanish);
+    const token = useStoreState(state => state.token);
+    const currentUser = useStoreState(state => state.currentUser);
 
-    return (
-        <div>
+    return <div>{renderHeader()}</div>;
+
+    function renderHeader() {
+        return (
             <Row>
                 <Col>
-                    <PageHeader title={spanish ? "Escuelas" : "Schools"} />
+                    <PageHeader title={spanish ? "Escuelas" : "Schools"}>
+                        {currentUser &&
+                            currentUser.clearance >= Clearance.ADMIN && (
+                                <Button size="sm" color={NEW_BUTTON_COLOR}>
+                                    New
+                                </Button>
+                            )}
+                    </PageHeader>
                 </Col>
             </Row>
-        </div>
-    );
+        );
+    }
 };

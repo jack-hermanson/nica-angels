a44c91153d32d938c0659267fa05c3ad0adf6e76
Jack Hermanson <jack.hermanson@live.com>
2021-09-17 09:20:27 -0600

    finish create edit student form

--- client/src/components/Students/CreateEditStudentForm.tsx
+++ client/src/components/Students/CreateEditStudentForm.tsx
@@ -9,7 +9,16 @@ import * as yup from "yup";
 import { FormError, LoadingSpinner } from "jack-hermanson-component-lib";
 import { Sex } from "jack-hermanson-ts-utils";
 import moment from "moment";
-import { Col, FormGroup, FormText, Input, Label, Row } from "reactstrap";
+import {
+    Button,
+    Col,
+    FormGroup,
+    FormText,
+    Input,
+    Label,
+    Row,
+} from "reactstrap";
+import { RESET_BUTTON_COLOR, SUBMIT_BUTTON_COLOR } from "../../utils/constants";
 
 interface Props {
     onSubmit: (studentRequest: StudentRequest) => Promise<void>;
@@ -45,7 +54,7 @@ export const CreateEditStudentForm: FunctionComponent<Props> = ({
             .number()
             .integer()
             .label(spanish ? "Sexo" : "Sex")
-            .required()
+            .required("Favor de registrar un sexo")
             .min(Sex.FEMALE)
             .max(Sex.MALE),
         level: yup
@@ -109,7 +118,7 @@ export const CreateEditStudentForm: FunctionComponent<Props> = ({
                     dateOfBirth: data.dateOfBirth
                         ? moment(data.dateOfBirth).toDate()
                         : undefined,
-                    sex: data.sex as Sex,
+                    sex: parseInt(data.sex as string),
                     level: data.level as number,
                     backpack: data.backpack,
                     shoes: data.shoes,
@@ -148,6 +157,12 @@ export const CreateEditStudentForm: FunctionComponent<Props> = ({
                                     {renderLevel(errors)}
                                 </Col>
                             </Row>
+                            <Row>
+                                <Col>{renderGeneralSupplies()}</Col>
+                            </Row>
+                            <Row>
+                                <Col>{renderButtons()}</Col>
+                            </Row>
                         </Fragment>
                     )}
                 </Form>
@@ -159,10 +174,10 @@ export const CreateEditStudentForm: FunctionComponent<Props> = ({
         const id = "first-name";
         return (
             <FormGroup>
-                <Label className="form-label required">
+                <Label className="form-label required" for={id}>
                     {spanish ? "Nombre" : "First Name"}
                 </Label>
-                <Field name="name" id={id} as={Input} autoFocus={true} />
+                <Field name="firstName" id={id} as={Input} autoFocus={true} />
                 <FormError>{errors.firstName}</FormError>
             </FormGroup>
         );
@@ -172,7 +187,7 @@ export const CreateEditStudentForm: FunctionComponent<Props> = ({
         const id = "middle-name";
         return (
             <FormGroup>
-                <Label className="form-label">
+                <Label className="form-label" for={id}>
                     {spanish ? "Segundo Nombre" : "Middle Name"}
                 </Label>
                 <Field name="middleName" id={id} as={Input} />
@@ -185,7 +200,7 @@ export const CreateEditStudentForm: FunctionComponent<Props> = ({
         const id = "last-name";
         return (
             <FormGroup>
-                <Label className="form-label">
+                <Label className="form-label" for={id}>
                     {spanish ? "Apellido" : "Last Name"}
                 </Label>
                 <Field name="lastName" id={id} as={Input} />
@@ -198,10 +213,10 @@ export const CreateEditStudentForm: FunctionComponent<Props> = ({
         const id = "date-of-birth";
         return (
             <FormGroup>
-                <Label className="form-label">
+                <Label className="form-label" for={id}>
                     {spanish ? "Fecha de Nacimiento" : "Date of Birth"}
                 </Label>
-                <Field type="date" name="last-name" id={id} as={Input} />
+                <Field type="date" name="dateOfBirth" id={id} as={Input} />
                 <FormError>{errors.dateOfBirth}</FormError>
             </FormGroup>
         );
@@ -211,7 +226,7 @@ export const CreateEditStudentForm: FunctionComponent<Props> = ({
         const id = "sex-input";
         return (
             <FormGroup>
-                <Label className="form-label required">
+                <Label for={id} className="form-label required">
                     {spanish ? "Sexo" : "Sex"}
                 </Label>
                 <Field type="select" name="sex" id={id} as={Input}>
@@ -234,7 +249,7 @@ export const CreateEditStudentForm: FunctionComponent<Props> = ({
         const id = "level-input";
         return (
             <FormGroup>
-                <Label className="form-label required">
+                <Label className="form-label required" for={id}>
                     {spanish ? "Nivel" : "Grade Level"}
                 </Label>
                 <Field
@@ -249,5 +264,65 @@ export const CreateEditStudentForm: FunctionComponent<Props> = ({
         );
     }
 
-    function renderSupplies() {}
+    function renderGeneralSupplies() {
+        return (
+            <FormGroup>
+                <Label className="form-label">
+                    {spanish ? "Suministros" : "Supplies"}
+                </Label>
+                {renderBackpack()}
+                {renderShoes()}
+                {renderSupplies()}
+            </FormGroup>
+        );
+    }
+
+    function renderBackpack() {
+        const id = "backpack-input";
+        return (
+            <FormGroup check>
+                <Field id={id} type="checkbox" name="backpack" as={Input} />
+                <Label className="form-check-label" for={id}>
+                    {spanish ? "Mochila" : "Backpack"}
+                </Label>
+            </FormGroup>
+        );
+    }
+
+    function renderShoes() {
+        const id = "shoes-input";
+        return (
+            <FormGroup check>
+                <Field id={id} type="checkbox" name="shoes" as={Input} />
+                <Label className="form-check-label" for={id}>
+                    {spanish ? "Zapatos" : "Shoes"}
+                </Label>
+            </FormGroup>
+        );
+    }
+
+    function renderSupplies() {
+        const id = "supplies-input";
+        return (
+            <FormGroup check>
+                <Field id={id} type="checkbox" name="supplies" as={Input} />
+                <Label className="form-check-label" for={id}>
+                    {spanish ? "Útiles" : "School Supplies"}
+                </Label>
+            </FormGroup>
+        );
+    }
+
+    function renderButtons() {
+        return (
+            <div className="bottom-buttons">
+                <Button color={SUBMIT_BUTTON_COLOR} type="submit">
+                    {spanish ? "Guardar" : "Save"}
+                </Button>
+                <Button color={RESET_BUTTON_COLOR} type="reset">
+                    {spanish ? "Restablecer" : "Reset"}
+                </Button>
+            </div>
+        );
+    }
 };

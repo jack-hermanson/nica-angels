559fc07d07b02d91fc00241fcf8ba169ff03bbaf
Jack Hermanson <jack.hermanson@live.com>
2022-01-01 15:58:58 -0700

    load user on edit user page

--- client/src/components/User/EditUser.tsx
+++ client/src/components/User/EditUser.tsx
@@ -1,17 +1,39 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useEffect, useState } from "react";
 import { RouteComponentProps } from "react-router-dom";
 import { SettingsTabs } from "../Settings/SettingsTabs";
 import { Col, Row } from "reactstrap";
-import { PageHeader } from "jack-hermanson-component-lib";
+import { LoadingSpinner, PageHeader } from "jack-hermanson-component-lib";
+import { useStoreState } from "../../store/_store";
+import { useMinClearance } from "../../utils/useMinClearance";
+import { AccountRecord, Clearance } from "@nica-angels/shared";
+import { AccountClient } from "../../clients/AccountClient";
 
 interface Props extends RouteComponentProps<{ id: string }> {}
 
 export const EditUser: FunctionComponent<Props> = ({ match }: Props) => {
+    const token = useStoreState(state => state.token);
+    const spanish = useStoreState(state => state.spanish);
+
+    const [user, setUser] = useState<AccountRecord | undefined>(undefined);
+
+    useMinClearance(Clearance.ADMIN);
+
+    useEffect(() => {
+        if (token) {
+            AccountClient.getAccount(
+                parseInt(match.params.id),
+                token.data
+            ).then(data => {
+                setUser(data);
+            });
+        }
+    }, [setUser, token]);
+
     return (
         <div>
             <SettingsTabs />
             {renderPageHeader()}
-            <p>For {match.params.id}</p>
+            {renderForm()}
         </div>
     );
 
@@ -19,8 +41,18 @@ export const EditUser: FunctionComponent<Props> = ({ match }: Props) => {
         return (
             <Row>
                 <Col>
-                    <PageHeader title="Edit User" />
+                    <PageHeader
+                        title={spanish ? "Editar Usuario" : "Edit User"}
+                    />
                 </Col>
+            </Row>
+        );
+    }
+
+    function renderForm() {
+        return (
+            <Row>
+                <Col>{user ? <p>Form here</p> : <LoadingSpinner />}</Col>
             </Row>
         );
     }

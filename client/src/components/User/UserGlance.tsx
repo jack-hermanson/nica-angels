f47edbf5da2282f3d28ba51f4ada83e3c0f8bb7e
Jack Hermanson <jack.hermanson@live.com>
2022-01-01 02:36:57 -0700

    show number of tokens on user glance

--- client/src/components/User/UserGlance.tsx
+++ client/src/components/User/UserGlance.tsx
@@ -1,8 +1,9 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useEffect, useState } from "react";
 import { Card } from "reactstrap";
 import { ActionCardHeader, KeyValCardBody } from "jack-hermanson-component-lib";
 import { useStoreState } from "../../store/_store";
 import { AccountRecord } from "@nica-angels/shared";
+import { AccountClient } from "../../clients/AccountClient";
 
 interface Props {
     account: AccountRecord;
@@ -10,6 +11,17 @@ interface Props {
 
 export const UserGlance: FunctionComponent<Props> = ({ account }: Props) => {
     const spanish = useStoreState(state => state.spanish);
+    const token = useStoreState(state => state.token);
+
+    const [numTokens, setNumTokens] = useState<number | undefined>(undefined);
+
+    useEffect(() => {
+        if (token) {
+            AccountClient.getTokens(account.id, token.data).then(data => {
+                setNumTokens(data);
+            });
+        }
+    }, [setNumTokens, token]);
 
     return (
         <Card className="mb-3 no-mb-last">
@@ -21,6 +33,10 @@ export const UserGlance: FunctionComponent<Props> = ({ account }: Props) => {
                     {
                         key: spanish ? "Correo Electrónico" : "Email",
                         val: account.email,
+                    },
+                    {
+                        key: "Tokens",
+                        val: numTokens === undefined ? "..." : numTokens,
                     },
                 ]}
             />

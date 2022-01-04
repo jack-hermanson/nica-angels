f48537cd8beb5b1206a370ae293508c279fbc01d
Jack Hermanson <jack.hermanson@live.com>
2022-01-03 23:52:52 -0700

    add dropdown menu

--- client/src/components/Students/CreateEditStudentForm.tsx
+++ client/src/components/Students/CreateEditStudentForm.tsx
@@ -1,5 +1,9 @@
-import { Fragment, FunctionComponent } from "react";
-import { StudentRecord, StudentRequest } from "@nica-angels/shared";
+import { Fragment, FunctionComponent, useEffect, useState } from "react";
+import {
+    SchoolRecord,
+    StudentRecord,
+    StudentRequest,
+} from "@nica-angels/shared";
 import { useStoreState } from "../../store/_store";
 import { Form, Formik, FormikErrors, FormikProps, Field } from "formik";
 import * as yup from "yup";
@@ -12,6 +16,7 @@ import {
     RESET_BUTTON_COLOR,
     SUBMIT_BUTTON_COLOR,
 } from "../../utils/constants";
+import { SchoolClient } from "../../clients/SchoolClient";
 
 interface Props {
     onSubmit: (studentRequest: StudentRequest) => Promise<void>;
@@ -23,6 +28,19 @@ export const CreateEditStudentForm: FunctionComponent<Props> = ({
     existingStudent,
 }: Props) => {
     const spanish = useStoreState(state => state.spanish);
+    const token = useStoreState(state => state.token);
+
+    const [schools, setSchools] = useState<SchoolRecord[] | undefined>(
+        undefined
+    );
+
+    useEffect(() => {
+        if (token) {
+            SchoolClient.getSchools(token.data).then(data => {
+                setSchools(data);
+            });
+        }
+    }, [token, setSchools]);
 
     const validationSchema = yup.object().shape({
         firstName: yup
@@ -155,14 +173,17 @@ export const CreateEditStudentForm: FunctionComponent<Props> = ({
                                 </Col>
                             </Row>
                             <Row>
-                                <Col xs={12} lg={4}>
+                                <Col xs={12} lg={3}>
                                     {renderDateOfBirth(errors)}
                                 </Col>
-                                <Col xs={12} lg={4}>
+                                <Col xs={12} lg={3}>
                                     {renderSex(errors)}
                                 </Col>
-                                <Col xs={12} lg={4}>
+                                <Col xs={12} lg={3}>
                                     {renderLevel(errors)}
+                                </Col>
+                                <Col xs={12} lg={3}>
+                                    {renderSchoolId(errors)}
                                 </Col>
                             </Row>
                             <Row>
@@ -330,6 +351,32 @@ export const CreateEditStudentForm: FunctionComponent<Props> = ({
                 <Label className="form-check-label" for={id}>
                     {spanish ? "Uniforme" : "Uniform"}
                 </Label>
+            </FormGroup>
+        );
+    }
+
+    function renderSchoolId(errors: FormikErrors<FormValues>) {
+        const id = "school-id-input";
+        return (
+            <FormGroup>
+                <Label className="form-label" for={id}>
+                    {spanish ? "Escuela" : "School"}
+                </Label>
+                <Field id={id} type="select" name="schoolId" as={Input}>
+                    <option>
+                        {spanish ? "Favor de elegir..." : "Please select..."}
+                    </option>
+                    {schools && (
+                        <Fragment>
+                            {schools.map(school => (
+                                <option value={school.id} key={school.id}>
+                                    {school.name}
+                                </option>
+                            ))}
+                        </Fragment>
+                    )}
+                </Field>
+                <FormError>{errors.schoolId}</FormError>
             </FormGroup>
         );
     }

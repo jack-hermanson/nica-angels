14c0615bcc17eee993909f7b81a77e68192f09fc
Jack Hermanson <jack.hermanson@live.com>
2022-01-31 09:55:06 -0700

    set up new sponsor page for later

---
+++ client/src/components/Sponsors/CreateSponsor.tsx
@@ -0,0 +1,37 @@
+import { FunctionComponent } from "react";
+import { useMinClearance } from "../../utils/useMinClearance";
+import { Clearance } from "@nica-angels/shared";
+import { Col, Row } from "reactstrap";
+import { PageHeader } from "jack-hermanson-component-lib";
+import { useStoreState } from "../../store/_store";
+
+export const CreateSponsor: FunctionComponent = () => {
+    const spanish = useStoreState(state => state.spanish);
+
+    return (
+        <div>
+            {renderPageHeader()}
+            {renderForm()}
+        </div>
+    );
+
+    function renderPageHeader() {
+        return (
+            <Row>
+                <Col>
+                    <PageHeader
+                        title={spanish ? "Nuevo Padrino" : "New Sponsor"}
+                    />
+                </Col>
+            </Row>
+        );
+    }
+
+    function renderForm() {
+        return (
+            <div>
+                <p>This is the form</p>
+            </div>
+        );
+    }
+};

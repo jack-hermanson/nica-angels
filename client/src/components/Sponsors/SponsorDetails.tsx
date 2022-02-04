868220201b4d02b7a312c94211a8295ded8f06e1
Jack Hermanson <jack.hermanson@live.com>
2022-02-03 17:07:09 -0700

    start sponsor detail page

--- client/src/components/Sponsors/SponsorDetails.tsx
+++ client/src/components/Sponsors/SponsorDetails.tsx
@@ -1,24 +1,40 @@
-import { FunctionComponent, useState } from "react";
+import { Fragment, FunctionComponent, useEffect, useState } from "react";
 import { RouteComponentProps } from "react-router-dom";
-import { PageHeader } from "jack-hermanson-component-lib";
-import { SponsorRecord } from "@nica-angels/shared";
+import { LoadingSpinner, PageHeader } from "jack-hermanson-component-lib";
+import { Clearance, SponsorRecord } from "@nica-angels/shared";
 import { Col, Row } from "reactstrap";
+import { useStoreState } from "../../store/_store";
+import { SponsorClient } from "../../clients/SponsorClient";
+import { useMinClearance } from "../../utils/useMinClearance";
+import { Link } from "react-router-dom";
+import { BUTTON_ICON_CLASSES, NEW_BUTTON_COLOR } from "../../utils/constants";
+import { FaPencilAlt } from "react-icons/fa";
 
 interface Props extends RouteComponentProps<{ id: string }> {}
 
 export const SponsorDetails: FunctionComponent<Props> = ({ match }: Props) => {
+    useMinClearance(Clearance.ADMIN);
+
     const [sponsor, setSponsor] = useState<SponsorRecord | undefined>(
         undefined
     );
 
+    const token = useStoreState(state => state.token);
+
+    useEffect(() => {
+        if (token) {
+            SponsorClient.getOne(parseInt(match.params.id), token.data).then(
+                data => {
+                    setSponsor(data);
+                }
+            );
+        }
+    }, [token, match.params.id, setSponsor]);
+
     return (
         <div>
             {renderPageHeader()}
-            <Row>
-                <Col>
-                    <p>This is for sponsor {match.params.id}</p>
-                </Col>
-            </Row>
+            {renderSponsorDetails()}
         </div>
     );
 
@@ -32,7 +48,32 @@ export const SponsorDetails: FunctionComponent<Props> = ({ match }: Props) => {
                                 ? `${sponsor.firstName} ${sponsor.lastName}`
                                 : "Sponsor Details"
                         }
-                    />
+                    >
+                        <Link
+                            className={`icon-button btn btn-sm btn-secondary`}
+                            to={`/sponsors/edit/${match.params.id}`}
+                        >
+                            <FaPencilAlt className={BUTTON_ICON_CLASSES} />
+                            Edit
+                        </Link>
+                    </PageHeader>
+                </Col>
+            </Row>
+        );
+    }
+
+    function renderSponsorDetails() {
+        return (
+            <Row>
+                <Col>
+                    {sponsor ? (
+                        <Fragment>
+                            {sponsor.firstName} {sponsor.lastName}{" "}
+                            {sponsor.email}
+                        </Fragment>
+                    ) : (
+                        <LoadingSpinner />
+                    )}
                 </Col>
             </Row>
         );

3140cdaf74be4020ee2005a26886f91fbe30acf8
Jack Hermanson <jack.hermanson@live.com>
2022-03-04 16:13:47 -0700

    extract sponsorship details into own component

--- client/src/components/Sponsorships/Sponsorship.tsx
+++ client/src/components/Sponsorships/Sponsorship.tsx
@@ -1,18 +1,16 @@
 import { Fragment, FunctionComponent, useEffect, useState } from "react";
 import {
-    getIdPadded,
     SponsorRecord,
     SponsorshipRecord,
     StudentRecord,
 } from "@nica-angels/shared";
 import { Card, CardBody, CardFooter, CardHeader } from "reactstrap";
-import { KeyValCardBody, LoadingSpinner } from "jack-hermanson-component-lib";
+import { LoadingSpinner } from "jack-hermanson-component-lib";
 import { useStoreState } from "../../store/_store";
 import { StudentClient } from "../../clients/StudentClient";
 import { SponsorClient } from "../../clients/SponsorClient";
 import { Link } from "react-router-dom";
-import { DATE_FORMAT, ID_PADDING } from "../../utils/constants";
-import moment from "moment";
+import { SponsorshipCardBody } from "./SponsorshipCardBody";
 
 interface Props {
     sponsorship: SponsorshipRecord;
@@ -88,45 +86,13 @@ export const Sponsorship: FunctionComponent<Props> = ({
 
     function renderDetails() {
         if (sponsor && student) {
-            const keyValPairs = [
-                {
-                    key: spanish ? "Estudiante" : "Student",
-                    val: (
-                        <Link className="ps-0" to={`/students/${student.id}`}>
-                            {student.firstName} {student.middleName}{" "}
-                            {student.lastName} (#
-                            {getIdPadded(student, ID_PADDING)})
-                        </Link>
-                    ),
-                },
-                {
-                    key: spanish ? "Padrino" : "Sponsor",
-                    val: (
-                        <Link className="ps-0" to={`/sponsors/${sponsor.id}`}>
-                            {sponsor.firstName} {sponsor.lastName}
-                        </Link>
-                    ),
-                },
-                {
-                    key: spanish ? "Pago" : "Payment",
-                    val: `$${sponsorship.payment}`,
-                },
-                {
-                    key: spanish ? "Frecuencia" : "Frequency",
-                    val: `${sponsorship.frequency}/${spanish ? "año" : "year"}`,
-                },
-                {
-                    key: spanish ? "Fecha de Comienza" : "Start Date",
-                    val: moment(sponsorship.startDate).format(DATE_FORMAT),
-                },
-            ];
-            if (sponsorship.endDate) {
-                keyValPairs.push({
-                    key: spanish ? "Fecha de Cierre" : "End Date",
-                    val: moment(sponsorship.endDate).format(DATE_FORMAT),
-                });
-            }
-            return <KeyValCardBody keyValPairs={keyValPairs} />;
+            return (
+                <SponsorshipCardBody
+                    sponsor={sponsor}
+                    student={student}
+                    sponsorship={sponsorship}
+                />
+            );
         }
     }
 };
---
+++ client/src/components/Sponsorships/SponsorshipCardBody.tsx
@@ -0,0 +1,66 @@
+import { FunctionComponent } from "react";
+import {
+    getIdPadded,
+    SponsorRecord,
+    SponsorshipRecord,
+    StudentRecord,
+} from "@nica-angels/shared";
+import { Link } from "react-router-dom";
+import { DATE_FORMAT, ID_PADDING } from "../../utils/constants";
+import moment from "moment";
+import { KeyValCardBody } from "jack-hermanson-component-lib";
+import { useStoreState } from "../../store/_store";
+
+interface Props {
+    sponsor: SponsorRecord;
+    student: StudentRecord;
+    sponsorship: SponsorshipRecord;
+}
+
+export const SponsorshipCardBody: FunctionComponent<Props> = ({
+    sponsor,
+    student,
+    sponsorship,
+}: Props) => {
+    const spanish = useStoreState(state => state.spanish);
+
+    const keyValPairs = [
+        {
+            key: spanish ? "Estudiante" : "Student",
+            val: (
+                <Link className="ps-0" to={`/students/${student.id}`}>
+                    {student.firstName} {student.middleName} {student.lastName}{" "}
+                    (#
+                    {getIdPadded(student, ID_PADDING)})
+                </Link>
+            ),
+        },
+        {
+            key: spanish ? "Padrino" : "Sponsor",
+            val: (
+                <Link className="ps-0" to={`/sponsors/${sponsor.id}`}>
+                    {sponsor.firstName} {sponsor.lastName}
+                </Link>
+            ),
+        },
+        {
+            key: spanish ? "Pago" : "Payment",
+            val: `$${sponsorship.payment}`,
+        },
+        {
+            key: spanish ? "Frecuencia" : "Frequency",
+            val: `${sponsorship.frequency}/${spanish ? "año" : "year"}`,
+        },
+        {
+            key: spanish ? "Fecha de Comienza" : "Start Date",
+            val: moment(sponsorship.startDate).format(DATE_FORMAT),
+        },
+    ];
+    if (sponsorship.endDate) {
+        keyValPairs.push({
+            key: spanish ? "Fecha de Cierre" : "End Date",
+            val: moment(sponsorship.endDate).format(DATE_FORMAT),
+        });
+    }
+    return <KeyValCardBody keyValPairs={keyValPairs} />;
+};

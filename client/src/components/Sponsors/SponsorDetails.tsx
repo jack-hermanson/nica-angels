828712527c8a7f928545ba7fefdaf20b6100a389
Jack Hermanson <jack.hermanson@live.com>
2022-03-04 17:44:16 -0700

    make list groups look nicer

--- client/src/components/Sponsors/SponsorDetails.tsx
+++ client/src/components/Sponsors/SponsorDetails.tsx
@@ -1,8 +1,23 @@
-import { FunctionComponent, useEffect, useState } from "react";
+import { Fragment, FunctionComponent, useEffect, useState } from "react";
 import { RouteComponentProps } from "react-router-dom";
 import { LoadingSpinner, PageHeader } from "jack-hermanson-component-lib";
-import { AccountRecord, Clearance, SponsorRecord } from "@nica-angels/shared";
-import { Col, Row } from "reactstrap";
+import {
+    AccountRecord,
+    Clearance,
+    SponsorRecord,
+    SponsorshipRecord,
+} from "@nica-angels/shared";
+import {
+    Card,
+    CardBody,
+    CardHeader,
+    Col,
+    ListGroup,
+    ListGroupItem,
+    ListGroupItemHeading,
+    ListGroupItemText,
+    Row,
+} from "reactstrap";
 import { useStoreState } from "../../store/_store";
 import { SponsorClient } from "../../clients/SponsorClient";
 import { useMinClearance } from "../../utils/useMinClearance";
@@ -11,6 +26,7 @@ import { BUTTON_ICON_CLASSES } from "../../utils/constants";
 import { FaPencilAlt } from "react-icons/fa";
 import { AccountClient } from "../../clients/AccountClient";
 import { SponsorDetailCard } from "./SponsorDetailCard";
+import { SponsorshipClient } from "../../clients/SponsorshipClient";
 
 interface Props extends RouteComponentProps<{ id: string }> {}
 
@@ -23,6 +39,9 @@ export const SponsorDetails: FunctionComponent<Props> = ({ match }: Props) => {
     const [account, setAccount] = useState<AccountRecord | undefined>(
         undefined
     );
+    const [sponsorships, setSponsorships] = useState<
+        SponsorshipRecord[] | undefined
+    >(undefined);
 
     const token = useStoreState(state => state.token);
 
@@ -39,16 +58,24 @@ export const SponsorDetails: FunctionComponent<Props> = ({ match }: Props) => {
                             setAccount(accountData);
                         });
                     }
+                    SponsorshipClient.getManyFromSponsorId(
+                        sponsorData.id,
+                        token.data
+                    ).then(sponsorshipData => {
+                        setSponsorships(sponsorshipData);
+                    });
                 }
             );
         }
-    }, [token, match.params.id, setSponsor, setAccount]);
+    }, [token, match.params.id, setSponsor, setAccount, setSponsorships]);
 
     return (
         <div>
             {renderPageHeader()}
-            {renderSponsorDetails()}
-            {renderSponsorships()}
+            <Row>
+                {renderSponsorDetails()}
+                {renderSponsorships()}
+            </Row>
         </div>
     );
 
@@ -78,26 +105,43 @@ export const SponsorDetails: FunctionComponent<Props> = ({ match }: Props) => {
 
     function renderSponsorDetails() {
         return (
-            <Row>
-                <Col>
-                    {sponsor ? (
-                        <SponsorDetailCard
-                            sponsor={sponsor}
-                            account={account}
-                        />
-                    ) : (
-                        <LoadingSpinner />
-                    )}
-                </Col>
-            </Row>
+            <Col xs={12} lg={8} className="mb-3 mb-lg-0">
+                {sponsor ? (
+                    <SponsorDetailCard sponsor={sponsor} account={account} />
+                ) : (
+                    <LoadingSpinner />
+                )}
+            </Col>
         );
     }
 
     function renderSponsorships() {
         return (
-            <Row className="mt-3">
-                <Col>To do: show sponsorships here</Col>
-            </Row>
+            <Col xs={12} lg={4}>
+                <Card>
+                    <CardHeader>
+                        <h5 className="mb-0">Sponsorships</h5>
+                    </CardHeader>
+                    <CardBody className="p-0">
+                        {sponsorships ? (
+                            <ListGroup flush>
+                                {sponsorships.map(sponsorship => (
+                                    <Fragment key={sponsorship.id}>
+                                        <ListGroupItem key={sponsorship.id}>
+                                            Test
+                                        </ListGroupItem>
+                                        <ListGroupItem key={sponsorship.id}>
+                                            Test
+                                        </ListGroupItem>
+                                    </Fragment>
+                                ))}
+                            </ListGroup>
+                        ) : (
+                            <LoadingSpinner />
+                        )}
+                    </CardBody>
+                </Card>
+            </Col>
         );
     }
 };

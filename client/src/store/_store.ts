edacef340701567bf8a980e7631e510370667833
Jack Hermanson <jack.hermanson@live.com>
2022-03-11 09:37:06 -0700

    show connecting to server message when socket is disconnected

--- client/src/store/_store.ts
+++ client/src/store/_store.ts
@@ -2,16 +2,19 @@ import { createStore, createTypedHooks } from "easy-peasy";
 import { settingsStore, SettingsStoreModel } from "./settingsStore";
 import { userStore, UserStoreModel } from "./userStore";
 import { alertStore, AlertStoreModel } from "./alertStore";
+import { connectionStore, ConnectionStoreModel } from "./connectionStore";
 
 export interface StoreModel
     extends SettingsStoreModel,
         UserStoreModel,
-        AlertStoreModel {}
+        AlertStoreModel,
+        ConnectionStoreModel {}
 
 export const store = createStore<StoreModel>({
     ...settingsStore,
     ...userStore,
     ...alertStore,
+    ...connectionStore,
 } as StoreModel);
 
 const typedHooks = createTypedHooks<StoreModel>();

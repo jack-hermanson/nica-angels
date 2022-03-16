2f3e9162ecf18884379912fb5c1e7660da0f3b1a
Jack Hermanson <jack.hermanson@live.com>
2022-03-16 14:15:14 -0600

    finish payment log service

--- server/src/services/PaymentLogService.ts
+++ server/src/services/PaymentLogService.ts
@@ -12,13 +12,48 @@ export abstract class PaymentLogService {
         };
     }
 
-    static async getAllFromPayment(paymentId: number) {
+    static async getAllFromPayment(paymentId: number): Promise<PaymentLog[]> {
         const { paymentLogRepo } = this.getRepos();
-        const paymentLogs = await paymentLogRepo.find({
+        return await paymentLogRepo.find({
             where: {
                 paymentId,
             },
-            order: {},
+            order: {
+                created: "DESC",
+            },
         });
+    }
+
+    static async getAll(): Promise<PaymentLog[]> {
+        const { paymentLogRepo } = this.getRepos();
+
+        return await paymentLogRepo.find({
+            order: {
+                created: "DESC",
+            },
+        });
+    }
+
+    static async create({
+        paymentId,
+        accountId,
+        ipAddress,
+        notes,
+    }: {
+        paymentId: number;
+        accountId: number;
+        ipAddress: string;
+        notes: string;
+    }): Promise<PaymentLog> {
+        const { paymentLogRepo } = this.getRepos();
+
+        const paymentLog = new PaymentLog();
+
+        paymentLog.paymentId = paymentId;
+        paymentLog.accountId = accountId;
+        paymentLog.ipAddress = ipAddress;
+        paymentLog.notes = notes;
+
+        return await paymentLogRepo.save(paymentLog);
     }
 }

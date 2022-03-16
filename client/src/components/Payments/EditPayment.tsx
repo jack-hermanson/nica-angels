a2762f3385198acd2c328fa9be07280932aea86f
Jack Hermanson <jack.hermanson@live.com>
2022-03-15 23:32:59 -0600

    edit payment from front end

--- client/src/components/Payments/EditPayment.tsx
+++ client/src/components/Payments/EditPayment.tsx
@@ -1,11 +1,14 @@
-import { RouteComponentProps } from "react-router-dom";
-import { FunctionComponent } from "react";
+import { RouteComponentProps, useHistory } from "react-router-dom";
+import { FunctionComponent, useEffect, useState } from "react";
 import { Col, Row } from "reactstrap";
-import { PageHeader } from "jack-hermanson-component-lib";
+import { LoadingSpinner, PageHeader } from "jack-hermanson-component-lib";
 import { useMinClearance } from "../../utils/useMinClearance";
-import { Clearance } from "@nica-angels/shared";
-import { useStoreState } from "../../store/_store";
+import { Clearance, PaymentRecord, PaymentRequest } from "@nica-angels/shared";
+import { useStoreActions, useStoreState } from "../../store/_store";
 import { SponsorTabs } from "../Sponsors/SponsorTabs";
+import { PaymentClient } from "../../clients/PaymentClient";
+import { errorAlert, scrollToTop, successAlert } from "jack-hermanson-ts-utils";
+import { CreateEditPaymentForm } from "./CreateEditPaymentForm";
 
 interface Props extends RouteComponentProps<{ id: string }> {}
 
@@ -13,6 +16,24 @@ export const EditPayment: FunctionComponent<Props> = ({ match }: Props) => {
     useMinClearance(Clearance.ADMIN);
 
     const spanish = useStoreState(state => state.spanish);
+    const token = useStoreState(state => state.token);
+    const addAlert = useStoreActions(actions => actions.addAlert);
+
+    const [payment, setPayment] = useState<PaymentRecord | undefined>(
+        undefined
+    );
+
+    const history = useHistory();
+
+    useEffect(() => {
+        if (token) {
+            PaymentClient.getOne(parseInt(match.params.id), token.data).then(
+                data => {
+                    setPayment(data);
+                }
+            );
+        }
+    }, [setPayment, token, match]);
 
     return (
         <div>
@@ -38,9 +59,37 @@ export const EditPayment: FunctionComponent<Props> = ({ match }: Props) => {
         return (
             <Row>
                 <Col>
-                    <p>This is the form</p>
+                    {payment ? (
+                        <CreateEditPaymentForm
+                            onSubmit={onSubmit}
+                            existingPayment={payment}
+                        />
+                    ) : (
+                        <LoadingSpinner />
+                    )}
                 </Col>
             </Row>
         );
+    }
+
+    async function onSubmit(paymentRequest: PaymentRequest) {
+        if (token) {
+            try {
+                const editedPayment = await PaymentClient.edit(
+                    parseInt(match.params.id),
+                    paymentRequest,
+                    token.data
+                );
+                addAlert(
+                    successAlert(`payment #${editedPayment.id}`, "edited")
+                );
+                history.push("/payments");
+            } catch (error: any) {
+                console.error(error);
+                console.error(error.response);
+                addAlert(errorAlert(error.message));
+                scrollToTop();
+            }
+        }
     }
 };

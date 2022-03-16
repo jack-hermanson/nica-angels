3c64a1e27bcb77682f7f1358cf2415a5966b124e
Jack Hermanson <jack.hermanson@live.com>
2022-03-16 15:49:16 -0600

    show payment logs and details

--- client/src/components/Payments/PaymentDetails.tsx
+++ client/src/components/Payments/PaymentDetails.tsx
@@ -1,11 +1,32 @@
 import { RouteComponentProps } from "react-router-dom";
-import { FunctionComponent } from "react";
+import { FunctionComponent, useEffect, useState } from "react";
 import { SponsorTabs } from "../Sponsors/SponsorTabs";
-import { PageHeader } from "jack-hermanson-component-lib";
-import { Col, Row } from "reactstrap";
+import {
+    ActionCardHeader,
+    LoadingSpinner,
+    PageHeader,
+} from "jack-hermanson-component-lib";
+import {
+    Card,
+    CardBody,
+    Col,
+    ListGroup,
+    ListGroupItem,
+    ListGroupItemHeading,
+    ListGroupItemText,
+    Row,
+} from "reactstrap";
 import { useMinClearance } from "../../utils/useMinClearance";
-import { Clearance } from "@nica-angels/shared";
+import {
+    Clearance,
+    ExpandedSponsorshipRecord,
+    PaymentRecord,
+} from "@nica-angels/shared";
 import { useStoreState } from "../../store/_store";
+import { PaymentClient } from "../../clients/PaymentClient";
+import { SponsorshipClient } from "../../clients/SponsorshipClient";
+import { PaymentCardBody } from "./PaymentCardBody";
+import moment from "moment";
 
 interface Props extends RouteComponentProps<{ id: string }> {}
 
@@ -13,12 +34,41 @@ export const PaymentDetails: FunctionComponent<Props> = ({ match }: Props) => {
     useMinClearance(Clearance.ADMIN);
 
     const spanish = useStoreState(state => state.spanish);
+    const token = useStoreState(state => state.token);
+
+    const [payment, setPayment] = useState<PaymentRecord | undefined>(
+        undefined
+    );
+    const [sponsorships, setSponsorships] = useState<
+        ExpandedSponsorshipRecord[] | undefined
+    >(undefined);
+
+    useEffect(() => {
+        if (token) {
+            PaymentClient.getOne(parseInt(match.params.id), token.data).then(
+                data => {
+                    setPayment(data);
+                }
+            );
+        }
+    }, [token, setPayment, match]);
+
+    useEffect(() => {
+        if (token) {
+            SponsorshipClient.getExpandedSponsorships(token.data).then(data => {
+                setSponsorships(data);
+            });
+        }
+    }, [token, setSponsorships]);
 
     return (
         <div>
             <SponsorTabs />
             {renderPageHeader()}
-            {renderInfo()}
+            <Row>
+                {renderDetailCard()}
+                {renderLogs()}
+            </Row>
         </div>
     );
 
@@ -34,13 +84,51 @@ export const PaymentDetails: FunctionComponent<Props> = ({ match }: Props) => {
         );
     }
 
-    function renderInfo() {
+    function renderDetailCard() {
         return (
-            <Row>
-                <Col>
-                    <p>Info</p>
-                </Col>
-            </Row>
+            <Col xs={12} lg={8} className="mb-3 mb-lg-0">
+                {payment ? (
+                    <Card>
+                        <PaymentCardBody
+                            payment={payment}
+                            sponsorships={sponsorships}
+                        />
+                    </Card>
+                ) : (
+                    <LoadingSpinner />
+                )}
+            </Col>
+        );
+    }
+
+    function renderLogs() {
+        return (
+            <Col xs={12} lg={4}>
+                {payment ? (
+                    <Card>
+                        <ActionCardHeader title="Logs" />
+                        <CardBody className="p-0">
+                            <ListGroup flush>
+                                {payment.paymentLogs?.map(log => (
+                                    <ListGroupItem key={log.id}>
+                                        <ListGroupItemHeading className="mb-1">
+                                            {moment(log.created).format("LLL")}
+                                        </ListGroupItemHeading>
+                                        <ListGroupItemText className="mb-0">
+                                            {log.notes}
+                                        </ListGroupItemText>
+                                        <ListGroupItemText className="mb-0 text-muted">
+                                            {log.ipAddress}
+                                        </ListGroupItemText>
+                                    </ListGroupItem>
+                                ))}
+                            </ListGroup>
+                        </CardBody>
+                    </Card>
+                ) : (
+                    <LoadingSpinner />
+                )}
+            </Col>
         );
     }
 };

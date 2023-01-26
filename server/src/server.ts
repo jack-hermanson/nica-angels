defdd64c63082acad5a9a79b04bded7d832ff88a
Jack Hermanson <jack.hermanson@live.com>
2023-01-25 22:01:56 -0700

    static redirect

--- server/src/server.ts
+++ server/src/server.ts
@@ -47,7 +47,7 @@ app.use("/api/payments", routes.payments);
 app.use("/api/reports", routes.reports);
 
 // production redirects
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === "test") {
     app.get("*", (req, res) => {
         res.sendFile(
             path.resolve(__dirname, "../../client/build", "index.html")
